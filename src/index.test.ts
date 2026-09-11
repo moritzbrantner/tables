50ffe143d6b096113c729850952f58172b06a1f9
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-11 19:24:41 +0200

    Record localization runtime export

--- src/index.test.ts
+++ src/index.test.ts
@@ -19,6 +19,7 @@ const expectedRuntimeExports = [
   "createTableServerQueryKey",
   "createTableServerRequest",
   "decodeTableViewState",
+  "defaultTableMessages",
   "encodeTableServerRequest",
   "encodeTableViewState",
   "getColumnValue",
